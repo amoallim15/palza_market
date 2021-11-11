5b86d0c87b9255cea6cd9833f8b44ea1a3558721
Ali Moallim <amoallim15@gmail.com>
2021-11-11 10:12:25 +0900

    Update migration.py

--- src/scripts/migration.py
+++ src/scripts/migration.py
@@ -1,2 +1,16 @@
+import motor.motor_asyncio
+
+
 def main():
+    url = "mongodb+srv://cluster0.9k0ry.mongodb.net/palza-market"
+    # 
+    db = motor.motor_asyncio.AsyncIOMotorClient(url)
+    print(db)
     pass
+
+
+
+
+
+if __name__ == "__main__":
+    main()
